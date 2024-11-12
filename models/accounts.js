feb151a8910d820be7df212efe07bb65d54ffc5b
radlee <prof.lolito@gmail.com>
2024-11-12 09:36:53 +0200

    Renamed Masxi

--- models/accounts.js
+++ models/accounts.js
@@ -2,7 +2,7 @@
 const accounts = [
   { accountNumber: '123456', balance: 5000, name: 'Troy ', surname: 'Carlo' },
   { accountNumber: '654321', balance: 10000, name: 'Jane', surname: 'Smith' },
-  { accountNumber: '987654', balance: 10000, name: 'Masizole', surname: 'Skunana' },
+  { accountNumber: '987654', balance: 10000, name: 'Masizole', surname: 'Sukwana' },
   { accountNumber: '111222', balance: 7500, name: 'Alex', surname: 'Brown' },
   { accountNumber: '333444', balance: 12000, name: 'Maria', surname: 'Lopez' }
 ];
