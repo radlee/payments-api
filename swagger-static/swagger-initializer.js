ea97f0fc49bc9633ee5aa3972f785b4e061dbb49
radlee <prof.lolito@gmail.com>
2024-11-17 14:17:26 +0200

    Some config

--- swagger-static/swagger-initializer.js
+++ swagger-static/swagger-initializer.js
@@ -1,24 +1,21 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // Determine the environment
-  const isProduction = window.location.hostname !== 'localhost';
+  const isProduction = window.location.hostname !== "localhost";
 
   // Configure the Swagger UI URL based on the environment
-  const swaggerUrl = isProduction ? 'https://payments-api-beta.vercel.app/static/swagger.json' : 'http://localhost:3000/swagger.json';
+  const swaggerUrl = isProduction
+    ? "https://payments-api-beta.vercel.app/swagger.json"
+    : "http://localhost:3000/swagger.json";
 
   window.ui = SwaggerUIBundle({
-    url:  swaggerUrl,  // Use the appropriate URL based on the environment
-    dom_id: '#swagger-ui',
+    url: swaggerUrl, // Use the appropriate URL based on the environment
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
